52845ac0f1709ff3a49b1e62a9499395457ed09c
Tanuj Soni <tanuj0997@gmail.com>
2020-07-03 18:08:24 +0530

    Added ansible and terraform charts

---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+provider "google" {
+  project = "apollo-task-282212"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "apollo_tt_instance"
+  machine_type = "f1-micro"
+
+  tags = ["test-webserver"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+
+  metadata = {
+    ssh-keys = "apollo:${file("~/.ssh/apollo.pub")}"
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "apollo-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "ssh"
+    ports    = "22"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "443"]
+  }
+
+  target_tags = ["test-webserver"]
+  source_ranges = ["104.56.114.248/32","198.144.216.128/32","103.129.121.173/32","192.195.81.38/32","103.253.151.66/32"]
+}
